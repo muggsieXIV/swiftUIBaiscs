3220014b3ffb926b738536aec4991acb3e763785
muggsieXIV <benwillwalk@gmail.com>
2021-01-18 10:41:46 -0700

    Initial Commit

---
+++ swiftUIBasics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUIBasics
+//
+//  Created by Bennie Walker on 1/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIBasics/swiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIBasicsApp.swift
+//  swiftUIBasics
+//
+//  Created by Bennie Walker on 1/18/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
